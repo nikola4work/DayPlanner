ab2355e80ef73fc12c9a915ae11a11391d8fb4a8
nikola <nikola4work@gmail.com>
2019-12-05 22:58:14 -0500

    added momentJS to display time and date

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
-// time and date display
-$("#dateTime").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+function getLocalStorage(key) {
+    var value = localStorage.getItem(key);
+    if (value) {
+        $("#text${key).text(value");
+    }
 
+}
 
-// document ready function
+// document ready
+
 $(document).ready(function() {
+        //moment js
+        $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
-    $(".fas").click(function() {
-        alert(
-            "click"
-        );
+        for (var i = 9; i < 18; i++) {
 
 
+            //create a row
+            var row = $(`<div data-time=${i} id='${i}' class="row">`);
+        }
     })
-
-
-
-
-
-
-});
-//  end of document ready function
+    // End of document ready brackets
